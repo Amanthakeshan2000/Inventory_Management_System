8fe169c08776af8f215f9b1bf0102a4045461056
Amanthakeshan2000 <amanthakeshangithub@gmail.com>
2024-07-05 23:37:45 +0530

    Initiated Frontend

---
+++ Frontend/inventory-management-system/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h1>Inventory Management System</h1>
+        
+    </div>
+  );
+}
+
+export default App;
