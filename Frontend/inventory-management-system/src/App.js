c7bf8edb2c86c718d0e7eead18cb9ca726dadca6
Amanthakeshan2000 <amanthakeshangithub@gmail.com>
2024-07-06 02:16:13 +0530

    1

--- Frontend/inventory-management-system/src/App.js
+++ Frontend/inventory-management-system/src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
-        <h1>NSBM Inventory Management System</h1>
-        
+      <h1>
+        INVENTORY MANAGEMENT SYSTEM
+      </h1>
     </div>
   );
 }
