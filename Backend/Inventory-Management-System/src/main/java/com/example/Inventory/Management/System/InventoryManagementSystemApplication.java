637105058850fa638127ac7b6b7ebf2d9bb5aa13
Amanthakeshan2000 <amanthakeshangithub@gmail.com>
2024-07-06 10:07:36 +0530

    Create Spingboot File

---
+++ Backend/Inventory-Management-System/src/main/java/com/example/Inventory/Management/System/InventoryManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.Inventory.Management.System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryManagementSystemApplication.class, args);
+	}
+
+}
---
+++ Backend/Inventory-Management-System/src/test/java/com/example/Inventory/Management/System/InventoryManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Inventory.Management.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
