c14ae12af0e7c628abe996a9e912dd2e7b538fbc
hettiarachchisp526 <subodhapriyathushara01@gmail.com>
2024-07-08 21:36:40 +0530

    create account test 01

--- Frontend/inventory-management-system/src/App.js
+++ Frontend/inventory-management-system/src/App.js
@@ -3,21 +3,16 @@ import React from 'react';
 import Sidebar from './Components/Sidebar';
 import Navbar from './Components/Navbar';
 import './App.css';
+import CreateAcountuserType from './Pages/CreateAcountuserType';
 
 const App = () => {
   return (
-    <div className="App">
-      <div className="container">
-        <Sidebar />
-        <div className="main-content">
-          <Navbar />
-          <div className="content">
-        
-              <h1>Your Page Here</h1>
-            
-          </div>
-        </div>
-      </div>
+    <div className="App1">
+      
+
+      <CreateAcountuserType/>
+
+
     </div>
   );
 };
---
+++ Frontend/inventory-management-system/src/Pages/CreateAcountuserType.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "../Css/createUserType.css"
+import "../Css/bootstrap.css"
+import createpageBgImage from"../Assets/createBgimage.png"
+
+export default function CreateAcountuserType() {
+  return (
+    <div className='select-userType-page'>
+      <div className='regBgImage'>
+        <img src={createpageBgImage} alt=''/>
+      </div>
+    </div>
+  )
+}
