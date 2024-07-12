3edc9fee9ba24851f99ecb52972fad0948470937
hettiarachchisp526 <subodhapriyathushara01@gmail.com>
2024-07-12 15:31:46 +0530

    finished-create-account-for-student

--- Frontend/inventory-management-system/src/App.js
+++ Frontend/inventory-management-system/src/App.js
@@ -4,13 +4,16 @@ import Sidebar from './Components/Sidebar';
 import Navbar from './Components/Navbar';
 import './App.css';
 import CreateAcountuserType from './Pages/CreateAcountuserType';
+import StudentCreateAccount from './Pages/StudentCreateAccount';
 
 const App = () => {
   return (
     <div className="App1">
       
 
-      <CreateAcountuserType/>
+      {/* <CreateAcountuserType/> */}
+
+      <StudentCreateAccount/>
 
 
     </div>
