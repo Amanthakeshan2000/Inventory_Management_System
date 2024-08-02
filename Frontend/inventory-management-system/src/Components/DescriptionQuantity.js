3941c610d74bc9f4637a391e9ec45f5af69bf1c5
kaveesha824 <kaveesha365@gmail.com>
2024-08-02 16:52:39 +0530

    change

--- Frontend/inventory-management-system/src/Components/DescriptionQuantity.js
+++ Frontend/inventory-management-system/src/Components/DescriptionQuantity.js
@@ -4,9 +4,9 @@ import '../Css/RequestForm.css';
 const DescriptionQuantity = () => {
   return (
     <div className="description-quantity">
-      <h5>Request Details</h5>
+      <h5 className='h5Color'>Request Details</h5>
       <div className="row">
-      <div className="form-group col-md-6">
+      <div className="form-group col-md-6" >
         <label htmlFor="description">Description</label>
         <textarea id="description" className="form-control" placeholder="Enter description for reason" maxLength="1000"></textarea>
       </div>
