790dacc16f60519a446785e7dfbfdfa521aa338e
kaveesha824 <kaveesha365@gmail.com>
2024-08-02 16:53:33 +0530

    change

---
+++ Frontend/inventory-management-system/src/Components/DescriptionQuantitySecond.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import '../Css/RequestForm.css';
+
+const DescriptionQuantity = () => {
+  return (
+    <div className="description-quantity">
+      <h5>Request Details</h5>
+      <div className="row">
+      <div className="form-group col-md-6">
+        <label htmlFor="description">Description</label>
+        <textarea id="description" className="form-control" placeholder="Enter description for reason" maxLength="1000"></textarea>
+      </div>
+      <div className="form-group col-md-6">
+        <label htmlFor="quantity">Quantity</label>
+        <input type="number" id="quantity" className="form-control" defaultValue="02" />
+      </div>
+      </div>
+
+    </div>
+  );
+};
+
+export default DescriptionQuantity;
