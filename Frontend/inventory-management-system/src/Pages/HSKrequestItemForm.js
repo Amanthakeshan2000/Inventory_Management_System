178f22bc9d844bcd5d29c6e12fd13b6249cab96a
hettiarachchisp526 <subodhapriyathushara01@gmail.com>
2024-08-04 10:20:16 +0530

    HSK buy req form update 3

--- Frontend/inventory-management-system/src/Pages/HSKrequestItemForm.js
+++ Frontend/inventory-management-system/src/Pages/HSKrequestItemForm.js
@@ -60,7 +60,107 @@ const CategoryItems = () => {
   );
 };
 
+// DateTime Component
+const DateTime = () => {
+  return (
+    <div className="date-time">
+      <br/>
+      <h5>Request date & time</h5>
+      <div className="row">
+        <div className="form-group col-md-6">
+          <label htmlFor="from-date">From</label>
+          <input type="date" id="from-date" className="form-control" />
+          <span className="note">Must request for a week.</span>
+        </div>
+        <div className="form-group col-md-6">
+          <label htmlFor="start-time">Start time</label>
+          <input type="time" id="start-time" className="form-control" />
+        </div>
+      </div>
+      <div className="row">
+        <div className="form-group col-md-6">
+          <label htmlFor="to-date">To</label>
+          <input type="date" id="to-date" className="form-control" />
+        </div>
+        <div className="form-group col-md-6">
+          <label htmlFor="end-time">End time</label>
+          <input type="time" id="end-time" className="form-control" />
+        </div>
+      </div>
+      <div className="form-group">
+        <br/>
+        <div className="radio-group">
+          <input type="radio" id="day" name="session" />
+          <label htmlFor="day">Day</label>
+          <input type="radio" id="night" name="session" defaultChecked />
+          <label htmlFor="night">Night</label>
+        </div>
+        <br/>
+      </div>
+      <br/>
+    </div>
+  );
+};
 
+// DescriptionQuantity Component
+const DescriptionQuantity = () => {
+  return (
+    <div className="description-quantity">
+      <h5>Request Details</h5>
+      <div className="row">
+        <div className="form-group col-md-6">
+          <label htmlFor="description">Description</label>
+          <textarea id="description" className="form-control" placeholder="Enter description for reason" maxLength="1000"></textarea>
+        </div>
+        <div className="form-group col-md-6">
+          <label htmlFor="quantity">Quantity</label>
+          <input type="number" id="quantity" className="form-control" defaultValue="02" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+// LocationPerson Component
+const LocationPerson = () => {
+  const categoryOptions = [
+    { value: 'electric', label: 'Electric' },
+    { value: 'furniture', label: 'Furniture' },
+  ];
+
+  return (
+    <div className="location-person">
+      <h5>Request Details</h5>
+      <div className="row">
+        <div className="form-group col-md-6">
+          <CustomSelect label="Select Faculty" options={categoryOptions} placeholder="Choose a Faculty" />
+        </div>
+        <div className="form-group col-md-6">
+          <CustomSelect label="Select Hall or Room" options={categoryOptions} placeholder="Choose a Hall or Room" />
+        </div>
+      </div>
+      <br/>
+      <div className="form-group">
+        <h5>Responsible Person</h5>
+        <div className="form-group">
+          <CustomSelect label="Select Responsible Person" options={categoryOptions} placeholder="Choose a Responsible Person" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+// CustomSelect Component
+const CustomSelect = ({ label, options, placeholder }) => {
+  return (
+    <div className="custom-select-container">
+      {label && <label className="custom-select-label">{label}</label>}
+      <Select 
+        className="custom-select"
+        options={options}
+        placeholder={placeholder}
+      />
+    </div>
   );
 };
 
