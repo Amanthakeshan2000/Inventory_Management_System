b93583325ef858d3cfa8e5a6425a7c0916f5bd26
Amanthakeshan2000 <amanthakeshangithub@gmail.com>
2024-09-04 23:28:00 +0530

    Modification

---
+++ Inventory-Management-System-Backend-v2/Inventory-Management-System/src/test/java/com/nsbm/Inventory_Management_System/InventoryManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nsbm.Inventory_Management_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
