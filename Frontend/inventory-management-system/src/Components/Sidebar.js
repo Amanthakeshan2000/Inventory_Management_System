7783e600a1a1589d9ec108a8f1275dbce2b82214
Dulmi-2000 <dulmiwijenayaka@gmail.com>
2024-09-14 01:22:58 +0530

    navigations added

--- Frontend/inventory-management-system/src/Components/Sidebar.js
+++ Frontend/inventory-management-system/src/Components/Sidebar.js
@@ -1,13 +1,18 @@
 import React, { useState } from 'react';
-import { FaBars, FaClipboardList, FaCalendarAlt, FaUsers, FaBuilding, FaUser, FaUndo } from 'react-icons/fa';
+import { FaBars, FaClipboardList, FaCalendarAlt, FaUsers, FaBuilding, FaUser, FaUndo,  FaFileInvoiceDollar  } from 'react-icons/fa';
+import { BsBoxFill } from "react-icons/bs";
 import '../Css/Sidebar.css';
 import logo from '../Assets/Logo.png';
 import '../Css/bootstrap.css';
+import { FaShop } from "react-icons/fa6";
+import { useNavigate } from 'react-router-dom';
 
 const Sidebar = () => {
   const [isRequestOpen, setIsRequestOpen] = useState(false);
   const [isEventsOpen, setIsEventsOpen] = useState(false);
   const [isSidebarOpen, setIsSidebarOpen] = useState(false); // Sidebar initially hidden
+
+  const navigate = useNavigate();
 
   const toggleRequestMenu = () => {
     setIsRequestOpen(!isRequestOpen);
@@ -33,7 +38,7 @@ const Sidebar = () => {
           <img src={logo} alt="NSBM Inventory Management System" />
         </div>
         <div className="menu">
-          <div className="menuItem">
+          <div className="menuItem" onClick={() => navigate('/')}>
             <FaBars className="icon" />
             <span>Dashboard</span>
           </div>
@@ -41,32 +46,44 @@ const Sidebar = () => {
             <FaClipboardList className="icon" />
             <span>Request</span>
           </div>
-          <div className={`submenu ${isRequestOpen ? 'open' : ''}`}>
+          <div className={`submenu ${isRequestOpen ? 'open' : ''}`} onClick={() => navigate('')}>
             <span>Create Request</span>
             <span>Request History</span>
           </div>
-          <div className="menuItem">
+          <div className="menuItem" onClick={() => navigate('')}>
             <FaUndo className="icon" />
             <span>Return</span>
           </div>
-          <div className="menuItem" onClick={toggleEventsMenu}>
+          <div className="menuItem" onClick={toggleEventsMenu} >
             <FaCalendarAlt className="icon" />
             <span>Events</span>
           </div>
-          <div className={`submenu ${isEventsOpen ? 'open' : ''}`}>
+          <div className={`submenu ${isEventsOpen ? 'open' : ''}`} onClick={() => navigate('')}>
             <span>Active Events</span>
           </div>
-          <div className="menuItem">
+          <div className="menuItem" onClick={() => navigate('')}>
             <FaUsers className="icon" />
             <span>Teams</span>
           </div>
-          <div className="menuItem">
+          <div className="menuItem" onClick={() => navigate('')}>
             <FaBuilding className="icon" />
             <span>Workplaces</span>
           </div>
-          <div className="menuItem">
+          <div className="menuItem" onClick={() => navigate('')}>
             <FaUser className="icon" />
             <span>Employees</span>
+          </div>
+          <div className="menuItem" onClick={() => navigate('')}>
+            <BsBoxFill className="icon" />
+            <span>Item</span>
+          </div>
+          <div className="menuItem" onClick={() => navigate('/Procurements')}>
+            < FaFileInvoiceDollar  className="icon"  />
+            <span>Procuremnets</span>
+          </div>
+          <div className="menuItem" onClick={() => navigate('/add-supplier')}>
+            <FaShop  className="icon" />
+            <span>Supplier</span>
           </div>
         </div>
       </div>
